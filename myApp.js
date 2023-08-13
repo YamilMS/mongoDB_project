5abed80e6277f65454d932c6ff04573324f8cd9e
Yamil <yoms.tf@gmail.com>
2023-08-13 13:30:35 +0100

    function findById()

--- myApp.js
+++ myApp.js
@@ -58,7 +58,10 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-
+  Person.findOne({favoriteFoods: food}, (err, personFound) =>{
+    if (err) return console.log(err);
+    done(null, personFound); 
+  })
 };
 
 const findPersonById = (personId, done) => {
