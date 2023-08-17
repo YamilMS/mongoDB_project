6fc9a499ba495963184d7332fdc1d6a9a3b1a160
Yamil <yoms.tf@gmail.com>
2023-08-17 09:50:14 +0100

    remove by id

--- myApp.js
+++ myApp.js
@@ -103,12 +103,14 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-  
+  Person.findByIdAndRemove(personId, (err, removedPerson) => {
+    if (err) return console.log(err);
+    done(null, removedPerson); 
+  })
 };
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
   done(null /*, data*/);
 };
 
